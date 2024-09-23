c5c5606e6c9c81cb20a5de7c83dabd97ebfadf85
Rohit Bhure <rohitbhure.cse@gmail.com>
2024-09-23 18:46:16 +0530

    learn

--- expressapp.js
+++ expressapp.js
@@ -2,18 +2,49 @@ const express = require('express')
 const app = express()
 const env = require('dotenv').config()
 const db = require('./mongoose')
-const port = process.env.PORT || 3000
 const fs = require('fs')
-// db(); // mongodb connection
+const morgan = require('morgan')
 
+db(); // mongodb connection
+const port = process.env.PORT || 3000
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
 
+// There are 5 types of Middle ware
+// * APPLICATON LEVEL MIDDLEWARE
+// * ROUTER LEVEL MIDDLEWARE
+// * ERROR-HANDLING MIDDLEWARE
+// * BUILT-IN MIDDLEWARE
+// * THIRD-PARTY MIDDLEWARE
+
+// Application level middleware
+app.use((req, res, next) => {
+  console.log(req.ip, req.hostname)
+  next()
+})
+
+// third party MiddleWare
+app.use(morgan('default'))
+
+// Built in middle ware use for understand json form data
+app.use(express.json()) // parses incoming requests with JSON payloads
+// app.use(express.static('public'))
+
+const auth = (req, res, next) => {
+  // if (req.query.pass == 123) { // req.query use for take paramiters from url
+  if (req.body.pass == 123) { // generally we use req.body for sending senstive data throught form
+    next()
+  } else {
+    res.sendStatus(401)
+  }
+}
+
 // API - ENDPOINTS - ROUTE
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => { // ROUTER LEVEL MIDDLEWARE
   res.header('Content-Type', 'application/json')
   res.json(data)
+  next()
 })
-app.post('/', (req, res) => {
+app.post('/', auth, (req, res) => { // set auth middleware only this
   res.json({ type: 'post' })
 })
 app.put('/', (req, res) => {
--- mongoose.js
+++ mongoose.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const env = require('dotenv').config()
 
-const DB = () =>{
+const DB = () => {
+    mongoose.set('strictQuery', true)
     mongoose.connect(process.env.MONGODB_CONNECT)
-    .then(() => {
-        console.log('MongoDB Connected...');
-    }).catch((err) => {
-        console.log('MongoDB Connection Error: ', err);
-    })
+        .then(() => {
+            console.log('MongoDB Connected...');
+        }).catch((err) => {
+            console.log('MongoDB Connection Error: ', err);
+        })
 }
 
 module.exports = DB
